d285780aeae5843b20e949e80c23b7a47806b9a2
IsmailBourbie <bourbieismail@gmail.com>
2019-11-15 15:40:44 +0100

    completing Mail.php class

--- app/Mail.php
+++ app/Mail.php
@@ -45,27 +45,77 @@ class Mail {
      * @param string $repleyTo_email 
      * @param string $repleyTo_name 
      */
-    public function setup_recipients (Array $recipients, Array $sender) {
-        $recipients = $this->doSomthing($recipients);
-        foreach($recipients as $email => $name){
+    public function setup_recipients ($email, $name) {
             if(trim($name) != "") {
                 $this->mailer->addAddress($email, $name);     // Add a recipient
             } else {
                 $this->mailer->addAddress($email);               // Name is optional
             }
-        }
-        $this->mailer->addReplyTo($sender["email"], $sender["name"]);
     }
 
     /**
      * setup the mail Content
-     * @
+     * @param Array $body
      */
-    public function setup_content(Array $body) {    
-        $this->mailer->isHTML($this->isHTML($body['data']));                                  // Set email format to HTML
+    public function setup_content(Array $body) {
+        // TODO: implement is_html() and setup_body() function 
+        $this->mailer->isHTML($this->is_html($body['data']));                                  // Set email format to HTML
         $this->mailer->Subject = $body['subject'];
-        $this->mailer->Body    = $this->setup_body($body['data'], $body['configuration']);
+        $this->mailer->Body    = $this->setup_body($body['type'], $body['data'], $body['configuration']);
         $this->mailer->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    }
+
+    /**
+     * setup the body information
+     * @param String $type; type of the mail (inline or file)
+     * @param String $data: the content of the body
+     * @param Array $config 
+     * @return String $message
+     */
+    private function setup_body($type, $data, $config) {
+        if($type === 'inline') {
+            $message = $data;
+        } else if($type === 'file') {
+            $message = file_get_contents($data);
+            $message = $this->setup_configuration($data, $config);
+        }
+        return $message;
+    }
+
+    /**
+     * setup the configuration of mail if is it html file
+     * @param Array $config
+     * @param string $data
+     */
+    private function setup_configuration($data, $config) {
+        // setup images
+        if($config['hasImages'] == 'true') {
+            $this->setup_images($config['images']);
+        }
+
+        // setup replacedText
+        if($config['hasReplacedText'] == 'true') {
+            $this->setup_keys($config['replacedKeys'], $data);
+        }
+    }
+
+    public function send_mails($recipients, $body) {
+        $recipients = $this->doSomthing($recipients);
+        foreach($recipients as $email => $name) {
+            $this->setup_recipients($email, $name);
+            $this->setup_content($body);
+            $this->send($email, $name);
+            $this->mailer->ClearAddresses();
+        }
+    }
+
+    private function send($email, $name) {
+        $tracing = [];
+        if(!$this->mailer->send()) {
+            $tracing['not_sent'][] = [$email, $name];
+        } else {
+            $tracing['sent'][] = [$email, $name];
+        }
     }
 
 
