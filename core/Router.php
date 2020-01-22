0f96bd0eb904aa0799d90c4e387240ac7763f9c8
IsmailBourbie <bourbieismail@gmail.com>
2020-01-22 11:50:22 +0100

    comment the code of Router class and delete similar class of request

--- core/Router.php
+++ core/Router.php
@@ -5,24 +5,47 @@ namespace Core;
 class Router
 {
 
+    /**
+     * all routes of the site
+     * @access private
+     */
     private $routes = [];
 
+
+    /**
+     * define the possible routes of the site
+     * @param array $routes
+     */
     public function define($routes)
     {
         $this->routes = $routes;
     }
 
 
+    /**
+     * create a router and load routes file
+     * @param strinf $file
+     * @return object $router
+     */
     public static function load ($file)
     {
         $router = new static;
         require $file;
         return $router;
     }
-    public function direct($uri)
+
+    /**
+     * direct the url to relative controller
+     * @param string $url
+     * @throws \Exeption
+     */
+    public function direct($url)
     {
-        if(array_key_exists($uri, $this->routes)) {
-            require $this->routes[$uri];
+        if(array_key_exists($url, $this->routes)) {
+            require $this->routes[$url];
+        } else {
+            die("No file found for this uri: $url");
+            
         }
     }
 
