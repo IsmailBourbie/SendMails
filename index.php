c88afd117d2444e51a68055f5e8f8a3ca0da5109
IsmailBourbie <bourbieismail@gmail.com>
2020-01-22 11:42:12 +0100

    refactoring: clean up index page

--- index.php
+++ index.php
@@ -4,13 +4,7 @@ use Core\Router;
 use Core\Request;
 
 require 'vendor/autoload.php';
-require "core/Router.php";
-$routes  = [
-    "" => "controllers/index.php",
-    "send" => "Controllers/send.php",
-    "configurations" => "controllers/configurations.php"
-];
-$router = new Router;
-$router->define($routes);
 
-$router->direct(Request::url());
+
+Router::load('routes.php')
+    ->direct(Request::url());
---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php
+
+$router->define(
+    [
+    "" => "controllers/index.php",
+    "send" => "Controllers/send.php",
+    "configurations" => "controllers/configurations.php"
+    ]
+);
